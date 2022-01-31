418519c2ac6f8a92053c63f309cd2d56e769bb1b
Xue Ying Lin <nikkiylin@gmail.com>
2022-01-30 21:40:07 -0800

    Use port 80

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-
 const express = require('express')
 
 const app = express()
-app.use(express.json());
-const port = 3000
+app.use(express.json())
+const port = 80
 
 var seed = 0
 
 app.get('/', (req, res) => {
-    
   res.send(`${seed}`)
 })
 
-app.post("/", (req, res) => {
-    seed = req.body.num 
-    res.send(`${seed}`)
-
+app.post('/', (req, res) => {
+  seed = req.body.num
+  res.send(`${seed}`)
 })
 
 app.listen(port, () => {
-
   console.log(`Example app listening on port ${port}`)
 })
