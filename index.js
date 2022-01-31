e5002b7c6b5a08b38921a78c471ef19afdbc8dfd
Xue Ying Lin <nikkiylin@gmail.com>
2022-01-30 21:24:10 -0800

    added files

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+
+const app = express()
+app.use(express.json());
+const port = 3000
+
+var seed = 0
+
+app.get('/', (req, res) => {
+    
+  res.send(`${seed}`)
+})
+
+app.post("/", (req, res) => {
+    seed = req.body.num 
+    res.send(`${seed}`)
+
+})
+
+app.listen(port, () => {
+
+  console.log(`Example app listening on port ${port}`)
+})
